cffe2d2f94eb9bec48907a5a7280fd92a27b925a
Jozef Wagner <jozef.wagner@gmail.com>
2015-03-19 11:05:01 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.dunaj/dunaj "0.3.7-pre2"]
+  :dependencies [[org.dunaj/dunaj "0.3.7"]
                  [org.asciidoctor/asciidoctorj "1.5.2"]])
