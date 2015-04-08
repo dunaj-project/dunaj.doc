67560b6fe8ae022566b98d0b2f4e186ef4c465b9
Jozef Wagner <jozef.wagner@gmail.com>
2015-04-08 22:13:23 +0200

    updatE

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject org.dunaj/dunaj.doc "0.1.0-SNAPSHOT"
+(defproject org.dunaj/dunaj.doc-lite "0.1.0-SNAPSHOT"
   :description "Dunaj documentation tools."
   :url "https://github.com/dunaj-project/dunaj.doc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.dunaj/dunaj "0.3.7"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.dunaj/dunaj-lite "0.3.8-SNAPSHOT"]
                  [org.asciidoctor/asciidoctorj "1.5.2"]])
