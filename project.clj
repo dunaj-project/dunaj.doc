855d2ee02737a499e97bc8ddbc956d5db9789825
Jozef Wagner <jozef.wagner@gmail.com>
2015-07-23 10:11:35 +0200

    update

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :profiles {:provided
-             {:dependencies
-              [[org.dunaj/dunaj "0.7.0-SNAPSHOT"]]}}
   :dependencies [[org.asciidoctor/asciidoctorj "1.5.2"]])
